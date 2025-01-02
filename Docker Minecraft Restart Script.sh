248c72c5799de4fa53df4d204ade9a12e3bd94cb
Nick Greenway <nickgreenway@icloud.com>
2025-01-02 18:33:45 -0500

    updated with red text to be more clear to players

--- Docker Minecraft Restart Script.sh
+++ Docker Minecraft Restart Script.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-/usr/local/bin/docker exec creative-mc-1 rcon-cli say SERVER RESTARTING in 20 seconds
+/usr/local/bin/docker exec INSERT_CONTAINER_NAME rcon-cli 'tellraw @a {"text":"SERVER RESTART INITIATED","bold":true,"color":"#FF2600"}'
+
+sleep 3
+
+/usr/local/bin/docker exec INSERT_CONTAINER_NAME rcon-cli 'tellraw @a ["",{"text":"SERVER RESTARTING:","bold":true,"color":"#FF2600"},{"text":" 20 seconds","color":"white"}]'
 
 sleep 17
 
-/usr/local/bin/docker exec creative-mc-1 rcon-cli say SERVER RESTARTING in 3 seconds
+/usr/local/bin/docker exec INSERT_CONTAINER_NAME rcon-cli 'tellraw @a ["",{"text":"SERVER RESTARTING:","bold":true,"color":"#FF2600"},{"text":" 3 seconds","color":"white"}]'
 
 sleep 1
 
-/usr/local/bin/docker exec creative-mc-1 rcon-cli say SERVER RESTARTING in 2 seconds
+/usr/local/bin/docker exec INSERT_CONTAINER_NAME rcon-cli 'tellraw @a ["",{"text":"SERVER RESTARTING:","bold":true,"color":"#FF2600"},{"text":" 2 seconds","color":"white"}]'
 
 sleep 1
 
-/usr/local/bin/docker exec creative-mc-1 rcon-cli say SERVER RESTARTING in 1 seconds
+/usr/local/bin/docker exec INSERT_CONTAINER_NAME rcon-cli 'tellraw @a ["",{"text":"SERVER RESTARTING:","bold":true,"color":"#FF2600"},{"text":" 1 seconds","color":"white"}]'
 
 sleep 1
 
-/usr/local/bin/docker exec creative-mc-1 rcon-cli save-all
+/usr/local/bin/docker exec INSERT_CONTAINER_NAME rcon-cli save-all
 
-/usr/local/bin/docker exec creative-mc-1 rcon-cli stop
+/usr/local/bin/docker exec INSERT_CONTAINER_NAME rcon-cli stop
